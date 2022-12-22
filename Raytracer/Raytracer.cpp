fb27b7aefcda090a5b815214834c25bb17401823
awegsch <a.wegscheider7141@gmail.com>
2022-12-22 17:38:14 +0100

    initial commit

---
+++ Raytracer/Raytracer.cpp
@@ -0,0 +1,12 @@
+﻿// Raytracer.cpp : Defines the entry point for the application.
+//
+
+#include "Raytracer.h"
+
+using std::cout, std::endl;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
